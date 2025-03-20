ec20103bd4e40939bd5fb1faf59467f7edae089f
Seixas <carlosseixasof@gmail.com>
2025-03-20 12:01:25 -0300

    Correção do Env Zod

--- src/env/zod.ts
+++ src/env/zod.ts
@@ -1,16 +1,17 @@
 import "dotenv/config";
 import { z } from "zod";
 
+const isTestEnv = process.env.NODE_ENV === "test";
+
 const envSchema = z.object({
-  DATABASE_URL: z.string(),
-  SECRET_WORD: z.string(),
+  DATABASE_URL: isTestEnv ? z.string().optional() : z.string(),
+  SECRET_WORD: z.string().default("teste"),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
   console.log("Invalid environment variables ❌", _env.error.format());
-
   throw new Error("Invalid environment variables ❌");
 }
 
